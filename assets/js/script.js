8b1a5d295c2f98b89f869b8e1b294538d92978e0
yalaouihicham <yalaouihicham13@gmail.com>
2022-07-07 23:39:33 +0200

    Last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // set little red dot above icons nav
 window.addEventListener("load", () => {
-  let checkbox = document.getElementsByTagName('input'); 
+  let checkbox = document.getElementsByTagName('input');
   let notif = document.getElementById('nav-not');
 
   // check if at least one of the checkboxes on the DOM is checked
-  const checkboxCheck = () => { 
+  const checkboxCheck = () => {
     for (let i = 0; i < checkbox.length; i++) {
       if (checkbox[i].checked) {
         return true;
@@ -15,7 +15,7 @@ window.addEventListener("load", () => {
 
   // set red dot
   const setNotif = () => {
-    if (checkboxCheck()){
+    if (checkboxCheck()) {
       // console.log('true');
       notif.style.visibility = 'visible';
       notif.style.opacity = '1';
@@ -25,7 +25,7 @@ window.addEventListener("load", () => {
       notif.style.opacity = '0';
     }
   }
-  
+
   // check once if red dot is needed
   setNotif();
 
